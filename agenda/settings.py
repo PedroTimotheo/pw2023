dbcbd6b07009e222ceff45a8478cd7dd2d2338fe
PedroTimotheo <pedrotimotheo953@gmail.com>
2023-06-02 07:51:07 -0300

    att3

--- agenda/settings.py
+++ agenda/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'agendaapp'
-    "crispy_forms",
+    'agendaapp',
+    'crispy_forms',
     "crispy_bootstrap5",
 ]
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
